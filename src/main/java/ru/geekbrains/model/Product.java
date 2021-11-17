c78df41792a0885ad4f2f6a0eb381c81d2bc62cd
Sbuchelov <mikivolkoff@ya.ru>
2021-11-17 11:34:51 +0300

    first homework

--- src/main/java/ru/geekbrains/model/Product.java
+++ src/main/java/ru/geekbrains/model/Product.java
@@ -2,10 +2,8 @@ package ru.geekbrains.model;
 
 import lombok.Data;
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-@Slf4j
 public class Product {
     @NonNull
     private int id;
